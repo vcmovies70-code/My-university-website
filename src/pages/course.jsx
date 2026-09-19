efc4685defa61a05441ac558ca5f1e53aa29f8f9
vcmovies70-code <vcmovies70@gmail.com>
2026-09-19 19:27:05 +0000

    fixed logo image in course

--- src/pages/course.jsx
+++ src/pages/course.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import contactlogoimg from "../assets/logo.png"
 
 const Course = () => {
   return (
@@ -7,7 +8,7 @@ const Course = () => {
      
     <section className="sub-header">
         <nav>
-            <a href="index.html"> <img src="./logo.png" alt=""/></a>
+            <a href="index.html"> <img src={contactlogoimg} alt=""/></a>
             <div className="nav-links" id="nav-links">
                 <i className="fa fa-times" onClick="hidemenu()"></i>
                 <ul>
